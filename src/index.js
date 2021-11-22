5519877c4e0ef4aae9d3246fa6f7f36b551c9f0b
dengninghai <dengninghai@shinemo.com>
2021-11-22 15:33:08 +0800

    feat：loading

--- src/index.js
+++ src/index.js
@@ -25,15 +25,15 @@ if (!window.__POWERED_BY_QIANKUN__) {
  * bootstrap 只会在微应用初始化的时候调用一次，下次微应用重新进入时会直接调用 mount 钩子，不会再重复触发 bootstrap。
  * 通常我们可以在这里做一些全局变量的初始化，比如不会在 unmount 阶段被销毁的应用级别的缓存等。
  */
-export async function bootstrap() {
-  console.log("[react17] react app bootstraped");
+export async function bootstrap(props) {
+  console.log("[react17] react app bootstraped", props.onLoading);
 }
 
 /**
  * 应用每次进入都会调用 mount 方法，通常我们在这里触发应用的渲染方法
  */
 export async function mount(props) {
-  console.log("[react17] props from main framework", props);
+  console.log("[react17] props from main framework", props.onLoading);
   render(props);
 }
 
